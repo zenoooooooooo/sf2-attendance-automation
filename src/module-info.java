c9c387eee12298517fa08838fd068332a3c78a47
lnxpsyhi <rommelmatic21@gmail.com>
2023-12-29 21:29:30 +0800

    gui integrated

---
+++ src/module-info.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module SF2_AUTOMATION_PROJECT {
+    
+    exports sf2_automation_project to javafx.graphics, javafx.fxml;
+    
+    requires javafx.swt;
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    requires java.base;
+}

17b0158be7e0fb49e923bcd42586298c2bbd6e8e
lnxpsyhi <rommelmatic21@gmail.com>
2024-01-01 01:13:16 +0800

    test

--- src/module-info.java
+++ src/module-info.java
@@ -40,7 +40,4 @@ module SF2_AUTOMATION_PROJECT {
     requires org.apache.commons.codec;
     requires commons.math3;
     requires SparseBitSet;
-    requires poi.javadoc;
-    requires commons.logging;
-
 }
